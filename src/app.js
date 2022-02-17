274979354592af73ee66091bb676582b5a2cff1c
seanstory-dev <seanstory.dev@gmail.com>
2022-02-17 10:21:24 +0900

    Init project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const carousel = ($container, images) => {};
+
+carousel(document.querySelector(".carousel"), [
+  "movies/movie-1.jpg",
+  "movies/movie-2.jpg",
+  "movies/movie-3.jpg",
+  "movies/movie-4.jpg",
+]);
